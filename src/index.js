7843897b69ebbd60aa9a680daf8180b548c66b07
Angel <a19angavimar@inspedralbes.cat>
2023-02-21 12:42:18 +0100

    server port

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+// setings
+app.set('port',process.env.PORT || 3000);
+
+//starting the server
+app.listen(app.get('port'), () =>{
+    console.log('Server on Port', app.get('port'));
+});
